471a534d097782ff3a8b07bd519fc12a05a039db
Ryan Calingo <gryan.calingo@gmail.com>
2018-02-18 23:52:38 -0500

    add server.js scripts

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
 
-const express = require('express');
-const method = require('method-override');
+var PORT = process.env.PORT || 3000;
 
-const app = express();
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
