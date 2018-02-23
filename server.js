37e5ac46ee7b32c002bf0d548485b91f028dea64
Ryan Calingo <gryan.calingo@gmail.com>
2018-02-23 07:41:07 -0500

    configure for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 3310;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(process.cwd() + '/public'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +23,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+app.use('/', routes);
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
